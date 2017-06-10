0210132f75c4d66657be3b9cfda38ef4e10e6bde
anthonysherwinchua <anthonysherwinchua@gmail.com>
2017-06-10 13:29:08 +0800

    setup seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+files = %w(
+
+)
+
+files.each do |file|
+  if file.singularize.camelize.constantize.count < 1
+    puts "File: #{file}"
+    require Rails.root + "db/seeds/#{file}.rb"
+  else
+    puts "Skipping File: #{file}"
+  end
+end
