725c4aa010c8c3a4b448c218a827dc9259cc730a
anthonysherwinchua <anthonysherwinchua@gmail.com>
2017-06-10 21:37:27 +0800

    use decorator for products; to convert the price / sale_price values from 100 to $1

--- app/controllers/v1/products_controller.rb
+++ app/controllers/v1/products_controller.rb
@@ -1,8 +1,8 @@
 class V1::ProductsController < ApplicationController
 
   def index
-    query = V1::ProductsQuery.new(products_params, params[:sort], page_params)
-    render json: query.call, each_serializer: ::V1::ProductSerializer
+    products = V1::ProductsQuery.new(products_params, params[:sort], page_params).call
+    render json: V1::ProductDecorator.wrap(products), each_serializer: ::V1::ProductSerializer
   end
 
   private
